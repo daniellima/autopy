7e1de40477a2c4973dae524c7a92330802b2f7a5
Daniel Lima <daniellima.pessoal@gmail.com>
2023-07-10 14:12:16 -0300

    new todos and move shortcuts to docker compose v2, since v1 is not installed anymore

--- zsh/docker_compose_alias.sh
+++ zsh/docker_compose_alias.sh
@@ -1,4 +1,4 @@
-alias dcu="docker-compose up -d"
-alias dcd="docker-compose down --remove-orphans"
-alias dcl="docker-compose logs -f --tail=100 $*"
-alias dc="docker-compose $*"
+alias dcu="docker compose up -d"
+alias dcd="docker compose down --remove-orphans"
+alias dcl="docker compose logs -f --tail=100 $*"
+alias dc="docker compose $*"
